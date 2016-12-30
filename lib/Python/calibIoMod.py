abaea2fe1f84598a77996123a4277bec0c5afed2
Logan Karsten <karsten@pequin.rap.ucar.edu>
2016-12-30 11:14:41 -0700

    Update 12/30/2016

--- lib/Python/calibIoMod.py
+++ lib/Python/calibIoMod.py
@@ -35,10 +35,7 @@ def setupModels(jobData):
     # the setup. 
     
     # First create top level directory based on the job name.
-    parentDir = jobData.outDir[0] + "/" + jobData.jobName[0]
-    print jobData.outDir
-    print jobData.outDir[0]
-    print jobData.jobName[0]
+    parentDir = jobData.outDir + "/" + jobData.jobName
     
     if os.path.isdir(parentDir):
         jobData.errMsg = "ERROR: Top level directory: " + parentDir + " already exists"
