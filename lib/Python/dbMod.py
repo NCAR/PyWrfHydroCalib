5c4c55ba78ce9611d95b61e1b91ece09bfa934f3
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-01-11 19:39:36 -0700

    Update 1/11/2017

--- lib/Python/dbMod.py
+++ lib/Python/dbMod.py
@@ -226,6 +226,6 @@ class Database(object):
         jobData.nCores = int(results[14])
         jobData.exe = results[15]
         jobData.nGages = int(results[16])
-        jobData.owner = results[17]
-        jobData.email = results[18]
+        jobData.email = results[17]
+        jobData.owner = results[18]
 
