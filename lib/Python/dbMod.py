eaf1ef34d74dee84aa8b3ce91a26e8a0a86134db
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-01-11 17:42:56 -0700

    Update 1/11/2017

--- lib/Python/dbMod.py
+++ lib/Python/dbMod.py
@@ -205,7 +205,7 @@ class Database(object):
             
         if not results:
             jobData.errMsg = "ERROR: No job data for matching ID of: " + str(jobData.jobID[0])
-            raise
+            raise Exception('TEST')
             
         # Fill jobData object with metadata on job and status.
         jobData.jobDir = results[1]
