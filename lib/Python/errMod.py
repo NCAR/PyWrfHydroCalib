68239773bc58da1e2a993a415968affec6e6da44
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-01-13 17:50:04 -0700

    Update 1/13/2017

--- lib/Python/errMod.py
+++ lib/Python/errMod.py
@@ -24,7 +24,7 @@ def errOut(jobData):
     if jobData.email:
         # Send error email out
         msg = MIMEText(msgContent)
-        emailTitle = "Errors in NWM Calibration for Job: " + jobData.jobName
+        emailTitle = "Errors in NWM Calibration for Job: " + jobData.jobID
         msg['Subject'] = emailTitle
         msg['From'] = jobData.email
         msg['To'] = jobData.email
@@ -33,7 +33,7 @@ def errOut(jobData):
         s.quit()
         sys.exit(1)
     elif jobData.slackObj:
-        msg1 = "ERROR in Job: " + jobData.jobName
+        msg1 = "ERROR in Job: " + jobData.jobID
         jobData.slackObj.chat.post_message(str(jobData.slChan),msg1,as_user=str(jobData.slUser))
         jobData.slackObj.chat.post_message(str(jobData.slChan),jobData.errMsg,as_user=str(jobData.slUser))
     else:
