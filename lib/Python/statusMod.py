e24d43ec27fa8b4631df3b34eba6d3164911c859
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-02-07 14:50:32 -0700

    Update 2/7/2017

--- lib/Python/statusMod.py
+++ lib/Python/statusMod.py
@@ -245,8 +245,12 @@ def walkMod(bDate,eDate,runDir):
         print hydroRestartPath
         if os.path.isfile(lsmRestartPath) and os.path.isfile(hydroRestartPath):
             bDate == dCurrent
+            print "FOUND RESTARTS"
+            print dCurrent
             print bDate
             print eDate
+            print bDate == eDate
+            print "END FOUND RESTARTS"
             #if hourModel == 0:
             #    # This implies the first time step of output is present. Get the expected
             #    # file size. This will be used to check to make sure the files present
