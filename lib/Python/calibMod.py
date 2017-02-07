02114c6b5bdbe860742af1b313269e0b30b8338e
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-02-07 12:42:47 -0700

    Update 2/7/2017

---
+++ lib/Python/calibMod.py
@@ -0,0 +1,407 @@
+# Module file for containing functions for executing WRF-Hydro model runs.
+
+# Logan Karsten
+# National Center for Atmospheric Research
+# Research Applications Laboratory
+
+#import datetime
+import os
+import calibIoMod
+import namelistMod
+import statusMod
+import errMod
+import subprocess
+
+def runModel(statusData,staticData,db,gageID,gage,keySlot,basinNum):
+    """
+    Generic function for running the model. Some basic information about
+    the run directory, beginning date, ending dates, account keys,
+    number of cores to use, etc will be used to compose a BSUB
+    submision script. This function will walk the run directory 
+    to determine where the model left off. If no restart files exist,
+    then the function will assume the model has not ran at all. Both
+    the LSM and hydro restart files must be present in order for the
+    model to restart. 
+    """
+    # Initialize iteration count. This is mostly used in the calibration looping, 
+    # but is 0.0 by default. 
+    iteration = 0
+    
+    # Determine which run sub-directory based on COMPLETE flag presence.
+    runDir = statusData.jobDir + "/" + gage + "/RUN.CALIB"
+    if not os.path.isdir(runDir):
+        statusData.errMsg = "ERROR: " + runDir + " not found."
+        raise Exception()
+        
+    # Check to make sure symbolic link to spinup state exists.
+    check1 = statusData.jobDir + "/" + gage + "/RUN.SPINUP/" + statusData.eSpinDate.strftime('%Y%m%d') + "00_DOMAIN1"
+    check2 = statusData.jobDir + "/" + gage + "/RUN.SPINUP/HYDRO_RST." + statusData.eSpinDate.strftime('%Y-%m-%d') + "_00:00_DOMAIN1"
+    if not os.path.isfile(check1):
+        statusData.errMsg = "ERROR: Spinup state: " + check1 + " not found."
+        raise Exception()
+    if not os.path.isfile(check2):
+        statusData.errMsg = "ERROR: Spinup state: " + check2 + " not found."
+        raise Exception()
+    # Create links if they don't exist
+    link1 = runDir + "/" + statusData.eSpinDate.strftime('%Y%m%d') + "00_DOMAIN1"
+    link2 = runDir + "/HYDRO_RST." + statusData.eSpinDate.strftime('%Y-%m-%d') + "_00:00_DOMAIN1"
+    if not os.path.islink(link1):
+        os.symlink(check1,link1)
+    if not os.path.islink(link2):
+        os.symlink(check2,link2)
+    # Generate BSUB file necessary for running R calibration/analysis
+    # code.
+    try:
+        generateCalibScript(statusData,int(gageID),runDir)
+    except:
+        raise
+        
+    # If BSUB run script doesn't exist, create it here.
+    bsubFile = runDir + "/run_NWM.sh"
+    if not os.path.isfile(bsubFile):
+        try:
+            generateRunScript(statusData,int(gageID),runDir)
+        except:
+            raise
+    
+    # Calculate datetime objects
+    begDate = statusData.bCalibDate
+    endDate = statusData.eCalibDate
+        
+    runFlag = 0
+    
+    # Pull gage metadata
+    gageMeta = calibIoMod.gageMeta()
+    try:
+        gageMeta.pullGageMeta(staticData,db,gage)
+    except:
+        raise
+        
+    # Determine the latest iteration that was completed. This will be logged
+    # Into a temporary table file created by R after 
+        
+    # Initialize status
+    keyStatus = keySlot[basinNum,iteration]
+    
+    try:
+        basinStatus = statusMod.checkBasJob(statusData,basinNum)
+    except:
+        raise
+        
+    print "BASIN STATUS = " + str(basinStatus)
+    # Create path to LOCK file if neeced
+    lockPath = runDir + "/RUN.LOCK"
+    
+    if keyStatus == 1.0:
+        # Model has already completed
+        runFlag = False
+        
+    # For uncompleted simulations that are still listed as running.
+    if keyStatus == 0.5:
+        # If a model is running for this basin, continue and set keyStatus to 0.5
+        if basinStatus:
+            keySlot[basinNum,iteration] = 0.5
+            keyStatus = 0.5
+            runFlag = False
+        else:
+            # Either simulation has completed, or potentially crashed.
+            runStatus = statusMod.walkMod(begDate,endDate,runDir)
+            begDate = runStatus[0]
+            endDate = runStatus[1]
+            runFlag = runStatus[2]
+            if runFlag:
+                # Model crashed as simulation is not complete but no processes are running.
+                statusData.genMsg = "WARNING: Simulation for gage: " + statusData.gages[basinNum] + \
+                                    " Failed. Attempting to restart."
+                print statusData.genMsg
+                errMod.sendMsg(statusData)
+                keySlot[basinNum] = -0.25
+                keyStatus = -0.25
+            else:
+                # Model has completed!
+                keySlot[basinNum,iteration] = 1.0
+                keyStatus = 1.0
+                runFlag = False
+           
+    print keyStatus
+    # For simulations that are fresh
+    if keyStatus == 0.0:
+        if basinStatus:
+            # Model is still running from previous instance of workflow. Allow it to continue.
+            keySlot[basinNum,iteration] = 0.5
+            keyStatus = 0.5
+            runFlag = False
+        else:
+            print begDate
+            print endDate
+            print "XXXXXX"
+            runStatus = statusMod.walkMod(begDate,endDate,runDir)
+            begDate = runStatus[0]
+            endDate = runStatus[1]
+            runFlag = runStatus[2]
+            print runFlag
+            print begDate
+            print endDate
+            if not runFlag:
+                # Model simulation completed before workflow was restarted
+                keySlot[basinNum,iteration] = 1.0
+                keyStatus = 1.0
+                runFlag = False
+                
+    # For when the model failed TWICE and is locked.
+    if keyStatus == -1.0:
+        # If LOCK file exists, no simulation will take place. File must be removed
+        # manually by user.
+        if os.path.isfile(lockPath):
+            runFlag = False
+        else:
+            # LOCK file was removed, upgrade status to 0.0 temporarily
+            runStatus = statusMod.walkMod(begDate,endDate,runDir)
+            begDate = runStatus[0]
+            endDate = runStatus[1]
+            runFlag = runStatus[2]
+            if runFlag:
+                keySlot[basinNum] = 0.0
+                keyStatus = 0.0
+            else:
+                # Model sucessfully completed.
+                keySlot[basinNum] = 1.0
+                keyStatus = 1.0
+                runFlag = False
+                
+    # For when the model crashed ONCE
+    if keyStatus == -0.5:
+        if basinStatus:
+            # Model is running again, upgrade status
+            # PLACEHOLDER FOR MORE ROBUST METHOD HERE.
+            keySlot[basinNum] = 0.5
+            keyStatus = 0.5
+            runFlag = False
+        else:
+            runStatus = statusMod.walkMod(begDate,endDate,runDir)
+            begDate = runStatus[0]
+            endDate = runStatus[1]
+            runFlag = runStatus[2]
+            if runFlag:
+                # Model has crashed again, time to lock it up and send a message out.
+                statusData.genMsg = "ERROR: SIMULATION FOR GAGE: " + statusData.gages[basinNum] + \
+                                    " HAS FAILED A SECOND TIME. PLEASE FIX ISSUE AND " + \
+                                    "MANUALLY REMOVE LOCK FILE: " + lockPath
+                errMod.sendMsg(statusData)
+                print statusData.genMsg
+                open(lockPath,'a').close()
+                keySlot[basinNum] = -1.0
+                keyStatus = -1.0
+                runFlag = False
+            else:
+                # Model sucessfully completed from first failed attempt.
+                keySlot[basinNum] = 1.0
+                keyStatus = 1.0
+                
+    if keyStatus == -0.25 and runFlag:
+        print keyStatus
+        # Restarting model from one crash
+        # First delete namelist files if they exist.
+        check = runDir + "/namelist.hrldas"
+        check2 = runDir + "/hydro.namelist"
+        if os.path.isfile(check):
+            os.remove(check)
+        if os.path.isfile(check2):
+            os.remove(check2)
+        
+        if begDate == staticData.bSpinDate:
+            startType = 1
+        else:
+            startType = 2
+        
+        try:
+            namelistMod.createHrldasNL(gageMeta,staticData,runDir,startType,begDate,endDate)
+            namelistMod.createHydroNL(gageMeta,staticData,runDir,startType,begDate,endDate)
+        except:
+            raise
+            
+        if startType == 2:
+            # Clean run directory of any old diagnostics files
+            try:
+                errMod.cleanRunDir(statusData,runDir)
+            except:
+                raise
+                
+        # Fire off model.
+        cmd = "bsub < " + runDir + "/run_NWM.sh"
+        print begDate
+        print endDate
+        try:
+            subprocess.call(cmd,shell=True)
+        except:
+            statusData.errMsg = "ERROR: Unable to launch NWM job for gage: " + str(gageMeta.gage[basinNum])
+            raise
+            
+        # Compile 
+        keyStatus = -0.5
+        keySlot[basinNum] = -0.5
+        
+    if keyStatus == 0.0 and runFlag:
+        # Model needs to be either ran, or restarted
+        # First delete namelist files if they exist.
+        check = runDir + "/namelist.hrldas"
+        check2 = runDir + "/hydro.namelist"
+        if os.path.isfile(check):
+            os.remove(check)
+        if os.path.isfile(check2):
+            os.remove(check2)
+        
+        if begDate == staticData.bSpinDate:
+            startType = 1
+        else:
+            startType = 2
+        
+        try:
+            namelistMod.createHrldasNL(gageMeta,staticData,runDir,startType,begDate,endDate)
+            namelistMod.createHydroNL(gageMeta,staticData,runDir,startType,begDate,endDate)
+        except:
+            raise
+            
+        if startType == 2:
+            # Clean run directory of any old diagnostics files
+            try:
+                errMod.cleanRunDir(statusData,runDir)
+            except:
+                raise
+                
+        # Fire off model.
+        cmd = "bsub < " + runDir + "/run_NWM.sh"
+        print cmd
+        try:
+            subprocess.call(cmd,shell=True)
+        except:
+            statusData.errMsg = "ERROR: Unable to launch NWM job for gage: " + str(gageMeta.gage[basinNum])
+            raise
+            
+        keyStatus = 0.5
+        keySlot[basinNum] = 0.5
+                
+def generateRunScript(jobData,gageID,runDir):
+    """
+    Generic function to create a run script that will be called by bsub
+    to execute the model.
+    """
+    
+    outFile = runDir + "/run_NWM.sh"
+    
+    if os.path.isfile(outFile):
+        jobData.errMsg = "ERROR: Run script: " + outFile + " already exists."
+        raise Exception()
+        
+    try:
+        fileObj = open(outFile,'w')
+        fileObj.write('#!/bin/bash\n')
+        fileObj.write('#\n')
+        fileObj.write('# LSF Batch Script to Run NWM Calibration Simulations\n')
+        fileObj.write('#\n')
+        inStr = "#BSUB -P " + str(jobData.acctKey) + '\n'
+        fileObj.write(inStr)
+        fileObj.write('#BSUB -x\n')
+        inStr = "#BSUB -n " + str(jobData.nCores) + '\n'
+        fileObj.write(inStr)
+        fileObj.write('#BSUB -R "span[ptile=16]"\n')
+        inStr = "#BSUB -J NWM_" + str(jobData.jobID) + "_" + str(gageID) + '\n'
+        fileObj.write(inStr)
+        inStr = '#BSUB -o ' + runDir + '/%J.out\n'
+        fileObj.write(inStr)
+        inStr = '#BSUB -e ' + runDir + '/%J.err\n'
+        fileObj.write(inStr)
+        fileObj.write('#BSUB -W 3:00\n')
+        fileObj.write('#BSUB -q premium\n')
+        fileObj.write('\n')
+        inStr = 'cd ' + runDir + '\n'
+        fileObj.write(inStr)
+        fileObj.write('mpirun.lsf ./wrf_hydro.exe\n')
+        fileObj.close
+    except:
+        jobData.errMsg = "ERROR: Failure to create: " + outFile
+        raise
+        
+def generateRScript(jobData,gageMeta,gageNum):
+    """
+    Generic function to create R script that will be sourced by R during
+    calibration.
+    """
+    outPath = jobData.outDir + "/" + jobData.jobName + "/" + str(jobData.gages[gageNum]) + \
+              "/RUN.CALIB/calibScript.R"
+    
+    if os.path.isfile(outPath):
+        jobData.errMsg = "ERROR: Calibration R script: " + outPath + " aleady exists."
+        raise Exception()
+
+    try:
+        fileObj = open(outPath,'w')
+        fileObj.write('#### Model Parameters ####\n')
+        inStr = "objFunc <- '" + str(jobData.objFunc) + "'\n"
+        fileObj.write(inStr)
+        fileObj.write('# Specify number of calibration iterations.\n')
+        inStr = "m <- " + str(jobData.nIter) + '\n'
+        fileObj.write(inStr)
+        fileObj.write('# Specify DDS parameter (if used).\n')
+        inStr = "r <- " + str(jobData.ddsR) + "\n"
+        fileObj.write(inStr)
+        fileObj.write("# Specify run directory containing calibration simulations.\n")
+        inStr = "runDir <- '" + jobData.outDir + "/" + jobData.jobName + "/" + \
+                str(jobData.gages[gageNum]) + "/RUN.CALIB'\n"
+        fileObj.write(inStr)
+        fileObj.write('# Parameter bounds\n')
+        fileObj.write('# Must create a data table called paramBnds with one row per parameter and columns labeled: \n')
+        fileObj.write('# "param" for parameter name, "ini" for initial value, "minValue" for minimum value, "maxValue" for maximum value\n')
+        inStr = "paramBnds <- read.table(paste0(runDir, '/calib_parms.tbl'), header=TRUE, sep=" ", stringsAsFactors=FALSE)\n"
+        fileObj.write('# Basin-Specific Metadata\n')
+        inStr = "siteId <- '" + str(jobData.gages[gageNum]) + "'\n"
+        fileObj.write(inStr)
+        inStr = "comId <- '" + str(gageMeta.comID) + "'\n"
+        fileObj.write(inStr)
+        fileObj.close
+    except:
+        jobData.errMsg = "ERROR: Failure to create: " + outPath
+        raise        
+        
+def generateCalibScript(jobData,gageID,runDir):
+    """
+    Generic Function function to create BSUB script for running R
+    calibration routines. These jobs will be shorter than 
+    the model runs, but still need to be ran through Yellowstone
+    compute nodes.
+    """
+    
+    outFile = runDir + "/run_NWM_CALIB.sh"
+    
+    if not os.path.isfile(outFile):
+        try:
+            fileObj = open(outFile,'w')
+            fileObj.write('#!/bin/bash\n')
+            fileObj.write('#\n')
+            fileObj.write('# LSF Batch Script to Run NWM Calibration R Code\n')
+            fileObj.write('#\n')
+            inStr = "#BSUB -P " + str(jobData.acctKey) + '\n'
+            fileObj.write(inStr)
+            #fileObj.write('#BSUB -x\n')
+            #inStr = "#BSUB -n " + str(jobData.nCores) + '\n'
+            #fileObj.write(inStr)
+            fileObj.write("#BSUB -n 1\n")
+            fileObj.write('#BSUB -R "span[ptile=1]"\n')
+            #fileObj.write('#BSUB -R "span[ptile=16]"\n')
+            inStr = "#BSUB -J NWM_CALIB" + str(jobData.jobID) + "_" + str(gageID) + '\n'
+            fileObj.write(inStr)
+            inStr = '#BSUB -o ' + runDir + '/%J.out\n'
+            fileObj.write(inStr)
+            inStr = '#BSUB -e ' + runDir + '/%J.err\n'
+            fileObj.write(inStr)
+            fileObj.write('#BSUB -W 3:00\n')
+            fileObj.write('#BSUB -q premium\n')
+            fileObj.write('\n')
+            inStr = 'cd ' + runDir + '\n'
+            fileObj.write(inStr)
+            #PLACEHOLDER FOR FIGURING OUT RUN SCRIPT
+            #fileObj.write('mpirun.lsf ./???\n')
+            fileObj.close
+        except:
+            jobData.errMsg = "ERROR: Failure to create: " + outFile
+        raise    
---
+++ lib/Python/spinupMod.py
@@ -0,0 +1,292 @@
+# Module file for containing functions for executing WRF-Hydro model runs.
+
+# Logan Karsten
+# National Center for Atmospheric Research
+# Research Applications Laboratory
+
+#import datetime
+import os
+import calibIoMod
+import namelistMod
+import statusMod
+import errMod
+import subprocess
+
+def runModel(statusData,staticData,db,gageID,gage,keySlot,basinNum):
+    """
+    Generic function for running the model. Some basic information about
+    the run directory, beginning date, ending dates, account keys,
+    number of cores to use, etc will be used to compose a BSUB
+    submision script. This function will walk the run directory 
+    to determine where the model left off. If no restart files exist,
+    then the function will assume the model has not ran at all. Both
+    the LSM and hydro restart files must be present in order for the
+    model to restart. 
+    """
+    runDir = statusData.jobDir + "/" + gage + "/RUN.SPINUP"
+    if not os.path.isdir(runDir):
+        statusData.errMsg = "ERROR: " + runDir + " not found."
+        raise Exception()
+        
+    # If BSUB run script doesn't exist, create it here.
+    bsubFile = runDir + "/run_NWM.sh"
+    if not os.path.isfile(bsubFile):
+        try:
+            generateRunScript(statusData,int(gageID),runDir)
+        except:
+            raise
+    
+    # Calculate datetime objects
+    begDate = statusData.bSpinDate
+    endDate = statusData.eSpinDate
+        
+    runFlag = 0
+    
+    # Pull gage metadata
+    gageMeta = calibIoMod.gageMeta()
+    try:
+        gageMeta.pullGageMeta(staticData,db,gage)
+    except:
+        raise
+        
+    # Initialize status
+    keyStatus = keySlot[basinNum]
+    
+    try:
+        basinStatus = statusMod.checkBasJob(statusData,basinNum)
+    except:
+        raise
+        
+    print "BASIN STATUS = " + str(basinStatus)
+    # Create path to LOCK file if neeced
+    lockPath = runDir + "/RUN.LOCK"
+    
+    if keyStatus == 1.0:
+        # Model has already completed
+        runFlag = False
+        
+    # For uncompleted simulations that are still listed as running.
+    if keyStatus == 0.5:
+        # If a model is running for this basin, continue and set keyStatus to 0.5
+        if basinStatus:
+            keySlot[basinNum] = 0.5
+            keyStatus = 0.5
+            runFlag = False
+        else:
+            # Either simulation has completed, or potentially crashed.
+            runStatus = statusMod.walkMod(begDate,endDate,runDir)
+            begDate = runStatus[0]
+            endDate = runStatus[1]
+            runFlag = runStatus[2]
+            if runFlag:
+                # Model crashed as simulation is not complete but no processes are running.
+                statusData.genMsg = "WARNING: Simulation for gage: " + statusData.gages[basinNum] + \
+                                    " Failed. Attempting to restart."
+                print statusData.genMsg
+                errMod.sendMsg(statusData)
+                keySlot[basinNum] = -0.25
+                keyStatus = -0.25
+            else:
+                # Model has completed!
+                keySlot[basinNum] = 1.0
+                keyStatus = 1.0
+                runFlag = False
+           
+    print keyStatus
+    # For simulations that are fresh
+    if keyStatus == 0.0:
+        if basinStatus:
+            # Model is still running from previous instance of workflow. Allow it to continue.
+            keySlot[basinNum] = 0.5
+            keyStatus = 0.5
+            runFlag = False
+        else:
+            print begDate
+            print endDate
+            print "XXXXXX"
+            runStatus = statusMod.walkMod(begDate,endDate,runDir)
+            begDate = runStatus[0]
+            endDate = runStatus[1]
+            runFlag = runStatus[2]
+            print runFlag
+            print begDate
+            print endDate
+            if not runFlag:
+                # Model simulation completed before workflow was restarted
+                keySlot[basinNum] = 1.0
+                keyStatus = 1.0
+                runFlag = False
+                
+    # For when the model failed TWICE and is locked.
+    if keyStatus == -1.0:
+        # If LOCK file exists, no simulation will take place. File must be removed
+        # manually by user.
+        if os.path.isfile(lockPath):
+            runFlag = False
+        else:
+            # LOCK file was removed, upgrade status to 0.0 temporarily
+            runStatus = statusMod.walkMod(begDate,endDate,runDir)
+            begDate = runStatus[0]
+            endDate = runStatus[1]
+            runFlag = runStatus[2]
+            if runFlag:
+                keySlot[basinNum] = 0.0
+                keyStatus = 0.0
+            else:
+                # Model sucessfully completed.
+                keySlot[basinNum] = 1.0
+                keyStatus = 1.0
+                runFlag = False
+                
+    # For when the model crashed ONCE
+    if keyStatus == -0.5:
+        if basinStatus:
+            # Model is running again, upgrade status
+            # PLACEHOLDER FOR MORE ROBUST METHOD HERE.
+            keySlot[basinNum] = 0.5
+            keyStatus = 0.5
+            runFlag = False
+        else:
+            runStatus = statusMod.walkMod(begDate,endDate,runDir)
+            begDate = runStatus[0]
+            endDate = runStatus[1]
+            runFlag = runStatus[2]
+            if runFlag:
+                # Model has crashed again, time to lock it up and send a message out.
+                statusData.genMsg = "ERROR: SIMULATION FOR GAGE: " + statusData.gages[basinNum] + \
+                                    " HAS FAILED A SECOND TIME. PLEASE FIX ISSUE AND " + \
+                                    "MANUALLY REMOVE LOCK FILE: " + lockPath
+                errMod.sendMsg(statusData)
+                print statusData.genMsg
+                open(lockPath,'a').close()
+                keySlot[basinNum] = -1.0
+                keyStatus = -1.0
+                runFlag = False
+            else:
+                # Model sucessfully completed from first failed attempt.
+                keySlot[basinNum] = 1.0
+                keyStatus = 1.0
+                
+    if keyStatus == -0.25 and runFlag:
+        print keyStatus
+        # Restarting model from one crash
+        # First delete namelist files if they exist.
+        check = runDir + "/namelist.hrldas"
+        check2 = runDir + "/hydro.namelist"
+        if os.path.isfile(check):
+            os.remove(check)
+        if os.path.isfile(check2):
+            os.remove(check2)
+        
+        if begDate == staticData.bSpinDate:
+            startType = 1
+        else:
+            startType = 2
+        
+        try:
+            namelistMod.createHrldasNL(gageMeta,staticData,runDir,startType,begDate,endDate)
+            namelistMod.createHydroNL(gageMeta,staticData,runDir,startType,begDate,endDate)
+        except:
+            raise
+            
+        if startType == 2:
+            # Clean run directory of any old diagnostics files
+            try:
+                errMod.cleanRunDir(statusData,runDir)
+            except:
+                raise
+                
+        # Fire off model.
+        cmd = "bsub < " + runDir + "/run_NWM.sh"
+        print begDate
+        print endDate
+        try:
+            subprocess.call(cmd,shell=True)
+        except:
+            statusData.errMsg = "ERROR: Unable to launch NWM job for gage: " + str(gageMeta.gage[basinNum])
+            raise
+            
+        # Compile 
+        keyStatus = -0.5
+        keySlot[basinNum] = -0.5
+        
+    if keyStatus == 0.0 and runFlag:
+        # Model needs to be either ran, or restarted
+        # First delete namelist files if they exist.
+        check = runDir + "/namelist.hrldas"
+        check2 = runDir + "/hydro.namelist"
+        if os.path.isfile(check):
+            os.remove(check)
+        if os.path.isfile(check2):
+            os.remove(check2)
+        
+        if begDate == staticData.bSpinDate:
+            startType = 1
+        else:
+            startType = 2
+        
+        try:
+            namelistMod.createHrldasNL(gageMeta,staticData,runDir,startType,begDate,endDate)
+            namelistMod.createHydroNL(gageMeta,staticData,runDir,startType,begDate,endDate)
+        except:
+            raise
+            
+        if startType == 2:
+            # Clean run directory of any old diagnostics files
+            try:
+                errMod.cleanRunDir(statusData,runDir)
+            except:
+                raise
+                
+        # Fire off model.
+        cmd = "bsub < " + runDir + "/run_NWM.sh"
+        print cmd
+        try:
+            subprocess.call(cmd,shell=True)
+        except:
+            statusData.errMsg = "ERROR: Unable to launch NWM job for gage: " + str(gageMeta.gage[basinNum])
+            raise
+            
+        keyStatus = 0.5
+        keySlot[basinNum] = 0.5
+                
+def generateRunScript(jobData,gageID,runDir):
+    """
+    Generic function to create a run script that will be called by bsub
+    to execute the model.
+    """
+    
+    outFile = runDir + "/run_NWM.sh"
+    
+    if os.path.isfile(outFile):
+        jobData.errMsg = "ERROR: Run script: " + outFile + " already exists."
+        raise Exception()
+        
+    try:
+        fileObj = open(outFile,'w')
+        fileObj.write('#!/bin/bash\n')
+        fileObj.write('#\n')
+        fileObj.write('# LSF Batch Script to Run NWM Calibration Simulations\n')
+        fileObj.write('#\n')
+        inStr = "#BSUB -P " + str(jobData.acctKey) + '\n'
+        fileObj.write(inStr)
+        fileObj.write('#BSUB -x\n')
+        inStr = "#BSUB -n " + str(jobData.nCores) + '\n'
+        fileObj.write(inStr)
+        fileObj.write('#BSUB -R "span[ptile=16]"\n')
+        inStr = "#BSUB -J NWM_" + str(jobData.jobID) + "_" + str(gageID) + '\n'
+        fileObj.write(inStr)
+        inStr = '#BSUB -o ' + runDir + '/%J.out\n'
+        fileObj.write(inStr)
+        inStr = '#BSUB -e ' + runDir + '/%J.err\n'
+        fileObj.write(inStr)
+        fileObj.write('#BSUB -W 3:00\n')
+        fileObj.write('#BSUB -q premium\n')
+        fileObj.write('\n')
+        inStr = 'cd ' + runDir + '\n'
+        fileObj.write(inStr)
+        fileObj.write('mpirun.lsf ./wrf_hydro.exe\n')
+        fileObj.close
+    except:
+        jobData.errMsg = "ERROR: Failure to create: " + outFile
+        raise
