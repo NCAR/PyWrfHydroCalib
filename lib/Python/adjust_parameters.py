f073b3e5cec0cc4aa518377bae0e676d46c9d0f9
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-02-08 17:11:59 -0700

    Update 2/8/17

---
+++ lib/Python/adjust_parameters.py
@@ -0,0 +1,82 @@
+# Program to adjust NWM parameter files per output from R calibration 
+# evaluations and adjustments. The following files will be adjusted:
+# 1.) Fulldom.nc
+# 2.) HYDRO.TBL
+# 3.) soil_properties.nc
+
+# Program is contingent on specific COMPLETE flag begin generated 
+# from R. If this file is not created, then this program will exit
+# gracefully without creating it's own COMPLETE flag. Without the 
+# Python-generated COMPLETE flag, the workflow will generate an error 
+# message. 
+
+# Logan Karsten
+# National Center for Atmospheric Research
+# Research Applications Laboratory
+# karsten@ucar.edu
+# 303-497-2693
+
+import argparse
+import sys
+from netCDF4 import Dataset
+import os
+import shutil
+
+def main(argv):
+    # Parse arguments. Only input necessary is the run directory.
+    parser = argparse.ArgumentParser(description='Main program to adjust input ' + \
+             'parameters for the National Water Model')
+    parser.add_argument('runDir',metavar='runDir',type=str,nargs='+',
+                        help='Run directory containing inputs necessary for adjustments.')
+                        
+    args = parser.parse_args()
+    runDir = str(args.runDir[0])
+    
+    # Compose input file paths.
+    fullDomOrig = runDir + "/BASELINE_PARAMETERS/Fulldom.nc"
+    hydroOrig = runDir + "/BASELINE_PARAMETERS/HYDRO.TBL"
+    soilOrig = runDir + "/BASELINE_PARAMETERS/soil_properties.nc"
+    rCompletePath = runDir + "/R_COMPLETE.R"
+    adjTbl = runDir + "/PARAMS_ADJUST.TBL"
+    
+    # Compose output file paths.
+    fullDomOut = runDir + "/Fulldom.nc"
+    hydroOut = runDir + "/HYDRO.TBL"
+    soilOut = runDir + "/soil_properties.nc"
+    outFlag = runDir + "/CALIB_ITER.COMPLETE"
+    
+    # If R COMPLETE flag not present, this implies the R code didn't run
+    # to completion.
+    if not os.path.isfile(rCompletePath):
+        sys.exit(1)
+        
+    os.remove(rCompletePath)
+    
+    try:
+        shutil.copy(fullDomOrig,fullDomOut)
+        shutil.copy(hydroOrig,hydroOut)
+        shutil.copy(soilOrig,soilOut)
+    except:
+        sys.exit(1)
+        
+    # Adjust HYDRO.TBL
+    
+    # Adjust Fulldom.nc
+    idAdj = Dataset(fullDomOut,'a')
+    
+    idAdj.close()
+    
+    # Adjust soil_properties.nc
+    idAdj = Dataset(soilOut,'a')
+    
+    idAdj.close()
+        
+    # Touch empty COMPLETE flag file. This will be seen by workflow, demonstrating
+    # calibration iteration is complete.
+    try:
+        open(outFlag,'a').close()
+    except:
+        sys.exit(1)
+        
+if __name__ == "__main__":
+    main(sys.argv[1:])
