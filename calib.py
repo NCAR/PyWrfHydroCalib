755fef736d31d1d50b6b0f2874b0273e219c1fdf
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-02-10 15:39:55 -0700

    Update 2/10/17

--- calib.py
+++ calib.py
@@ -248,7 +248,7 @@ def main(argv):
         for basin in range(0,len(jobData.gages)):
             for iteration in range(0,int(jobData.nIter)):
                 calibMod.runModel(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin,iteration)
-                #time.sleep(30)
+                time.sleep(15)
                 print "MOVING ALONG"
     #        #try:
     #        #    calibMod.runModel(jobData,staticData,db,jobData.gages[basin],keySlot,basin)
