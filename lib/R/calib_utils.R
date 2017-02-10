bca254aa65bd404a5dd3a880cf5ddd0c4e3a1c68
Logan Karsten <karsten@vpn49.ucar.edu>
2017-02-09 20:19:18 -0700

    Update 2/9/17

---
+++ lib/R/calib_utils.R
@@ -0,0 +1,183 @@
+###----------------- UTILITIES -------------------###
+
+# Namelist read function
+
+ReadNamelist <- function(nlist) {
+   source(nlist)
+   #load(obsFile)
+}
+
+# Convert to daily flow
+
+Convert2Daily <- function(str) {
+   str$Date <- rwrfhydro::CalcDateTrunc(str$POSIXct)
+   setkey(str, Date)
+   str.d <- str[, list(q_cms=mean(q_cms, na.rm=TRUE)), by = "Date"]
+   str.d$POSIXct <- as.POSIXct(paste0(str.d$Date, " 00:00"), tz="UTC")
+   str.d
+}
+
+# Read streamflow from netcdf file
+
+ReadChFile <- function(file, idList){
+    nc <- ncdf4::nc_open(file)
+    output <- data.frame(q_cms = ncdf4::ncvar_get(nc, varid = "streamflow", start = idList , count =1),
+                         POSIXct = as.POSIXct(strsplit(basename(file),"[.]")[[1]][1], format = "%Y%m%d%H%M", tz = "UTC"))
+    ncdf4::nc_close(nc)
+    return(output)
+}
+
+###----------------- OPTIMIZATION -------------------###
+
+# DDS parameter selection function
+
+DDS.sel <- function(i, m, r, xnames, x_min, x_max, x_best) {
+
+   # Set parameter set
+   P_i <- 1-log(i)/log(m)
+   sel <- c()
+   for (d in 1:length(xnames)) {
+      sel[d] <- sample(c(1,0), 1, prob=c(P_i, 1-P_i))
+   }
+   N <- xnames[as.logical(sel)]
+   if (length(N) < 1) N <- sample(xnames, 1)
+
+   # Set new values for selected parameters
+   x_new <- x_best
+   for (j in N) {
+      xj_min <- x_min[[j]]
+      xj_max <- x_max[[j]]
+      xj_best <- x_best[[j]]
+      sigj <- r * (xj_max - xj_min)
+      x_new[[j]] <- xj_best + sigj*rnorm(1)
+      if (x_new[[j]] < xj_min) {
+         x_new[[j]] <- xj_min + (xj_min - x_new[[j]])
+         if (x_new[[j]] > xj_max) {
+            x_new[[j]] <- xj_min
+         }
+      }
+      if (x_new[[j]] > xj_max) {
+         x_new[[j]] <- xj_max - (x_new[[j]] - xj_max)
+         if (x_new[[j]] < xj_min) {
+            x_new[[j]] <- xj_max
+         }
+      }
+   }
+   x_new
+
+}
+
+###----------------- METRICS -------------------###
+
+# RMSE
+Rmse <- function (m, o, na.rm=TRUE) {
+    err <- sum((m - o)^2, na.rm=na.rm)/(min(sum(!is.na(m)),sum(!is.na(o))))
+    rmserr <- sqrt(err)
+    rmserr
+}
+
+# Percent Bias
+PBias <- function (m, o, na.rm=TRUE) {
+    sum(m - o, na.rm=na.rm)/sum(o, na.rm=na.rm) * 100
+}
+
+# NSE
+Nse <- function (m, o, nullModel=mean(o, na.rm=na.rm), na.rm=TRUE) {
+    err1 <- sum((m - o)^2, na.rm=na.rm)
+    err2 <- sum((o - nullModel)^2, na.rm=na.rm)
+    ns <- 1 - (err1/err2)
+    ns
+}
+
+# LogNSE
+NseLog <- function (m, o, nullModel=mean(o, na.rm=na.rm), na.rm=TRUE) {
+    m <- log(m + 1e-04)
+    o <- log(o + 1e-04)
+    err1 <- sum((m - o)^2, na.rm=na.rm)
+    err2 <- sum((o - nullModel)^2, na.rm=na.rm)
+    ns <- 1 - (err1/err2)
+    ns
+}
+
+# Weighted NSE LogNSE
+NseWt <- function (m, o, w=0.5, p=1) { 
+    # NSE
+    err1 <- sum((m - o)^2, na.rm=T)
+    err2 <- sum((o - mean(o, na.rm=T))^2, na.rm=T)
+    nse <- 1 - (err1/err2)
+    # Ln NSE
+    lnm <- log(m + 1e-04)
+    lno <- log(o + 1e-04)
+    err1 <- sum((lnm - lno)^2, na.rm=T)
+    err2 <- sum((lno - mean(lno, na.rm=T))^2, na.rm=T)
+    lnnse <- 1 - (err1/err2)
+    # Weighted mean
+    res <- ((w^p) * (nse^p) + (w^p) * (lnnse^p))^(1/p)
+}
+
+# KGE
+Kge <- function (m, o, na.rm=TRUE, s.r=1, s.alpha=1, s.beta=1) {
+  use <- if(na.rm) 'pairwise.complete.obs' else 'everything'
+  r     <- cor(m, o, use=use)
+  alpha <- sd(m, na.rm=na.rm) / sd(o, na.rm=na.rm)
+  beta  <- mean(m, na.rm=na.rm) / mean(o, na.rm=na.rm)
+  eds = sqrt( (s.r*(r-1))^2 + (s.alpha*(alpha-1))^2 + (s.beta*(beta-1))^2 )
+  kges = 1-eds
+}
+
+
+###----------------- PLOTTING -------------------###
+
+# ggplot color palette
+gg_color_hue <- function(n) {
+   hues = seq(15, 375, length = n + 1)
+   hcl(h = hues, l = 65, c = 100)[1:n]
+ }
+
+# Multiplot
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
