59d48901ed12654c139b7fbc257c8170ad6d49e2
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-02-14 10:42:36 -0700

    Update 2/14/17

--- jobInit.py
+++ jobInit.py
@@ -86,18 +86,16 @@ def main(argv):
         errMod.errOut(jobData)
         
     # Create necessary run directories to hold output, analysis, etc.
-    calibIoMod.setupModels(jobData,db,args)
-    #try:
-    #    calibIoMod.setupModels(jobData,db,args)
-    #except:
-    #    errMod.errOut(jobData)
+    try:
+        calibIoMod.setupModels(jobData,db,args)
+    except:
+        errMod.errOut(jobData)
        
     # Create DB entries for job name
-    db.enterJobID(jobData)
-    #try:
-    #    db.enterJobID(jobData)
-    #except:
-    #    errMod.errOut(jobData)
+    try:
+        db.enterJobID(jobData)
+    except:
+        errMod.errOut(jobData)
         
     # Pull Job ID from newly created job. Will be used for calibration 
     # parameter DB entries
