810a1679e5a5d7929b435bae68f321665881ce70
Logan Karsten <karsten@vpn32.ucar.edu>
2017-02-14 23:30:35 -0700

    Update 2/14/2017

--- getCalibReport.py
+++ getCalibReport.py
@@ -104,14 +104,18 @@ def main(argv):
                 msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
                          " - IS READY TO BEGIN ITERATION: " + str(iteration+1) + "\n " 
             else:
-                print '------------------'
-                print keyStatusPrev
-                print iteration
-                print keyStatus
+                #print '------------------'
+                #print keyStatusPrev
+                #print iteration
+                #print keyStatus
                 if keyStatusPrev == 0.0 and iteration == 0 and keyStatus == 0.0:
                     msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
                              " - HAS NOT BEGUN CALIBRATION.\n"
                 if keyStatusPrev == 1.0 and iteration != 0:
+                    msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
+                             ": " + str(msgDict[str(keyStatus)]) + \
+                             " - ITERATION: " + str(iteration+1) + "\n"
+                if keyStatusPrev == 0.0 and iteration == 0 and keyStatus != 0.0:
                     msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
                              ": " + str(msgDict[str(keyStatus)]) + \
                              " - ITERATION: " + str(iteration+1) + "\n"
