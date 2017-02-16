95a18b09a793d130d380970d4c3aaad463a56b65
Logan Karsten <karsten@vpn55.ucar.edu>
2017-02-15 22:17:11 -0700

    Update 2/15/17

--- lib/Python/statusMod.py
+++ lib/Python/statusMod.py
@@ -144,31 +144,28 @@ def checkYsJobs(jobData):
 
     # Loop through and check to make sure no existing jobs are being ran for any 
     # of the gages.
-    print str(jobData.owner)
-    print str(userTmp)
-    if str(jobData.owner) != str(userTmp):
-        if len(jobs) != 0:
-            for gageCheck in range(0,len(jobData.gageIDs)):
-                jobNameCheck = "NWM_" + str(jobData.jobID) + "_" + str(jobData.gageIDs[gageCheck])
-                jobNameCheck2 = "NWM_CALIB_" + str(jobData.jobID) + "_" + str(jobData.gageIDs[gageCheck])
-                testDF = jobs.query("JOB_NAME == '" + jobNameCheck + "'")
-                if len(testDF) != 0:
-                    jobData.errMsg = "ERROR: Job ID: " + str(jobData.jobID) + \
-                                     " is already being ran under owner: " + \
-                                     str(jobData.owner) + ". User: " + \
-                                     str(userTmp) + " is attempting to initiate the workflow."
-                    print "ERROR: You are attempting to initiate a job that is already being " + \
-                          "ran by user: " + str(jobData.owner)
-                    raise Exception()
-                testDF = jobs.query("JOB_NAME == '" + jobNameCheck2 + "'")
-                if len(testDF) != 0:
-                    jobData.errMsg = "ERROR: Job ID: " + str(jobData.jobID) + \
-                                     " is already being ran under owner: " + \
-                                     str(jobData.owner) + ". User: " + \
-                                     str(userTmp) + " is attempting to initiate the workflow."
-                    print "ERROR: You are attempting to initiate a job that is already being " + \
-                          "ran by user: " + str(jobData.owner)
-                    raise Exception()
+    if len(jobs) != 0:
+        for gageCheck in range(0,len(jobData.gageIDs)):
+            jobNameCheck = "NWM_" + str(jobData.jobID) + "_" + str(jobData.gageIDs[gageCheck])
+            jobNameCheck2 = "NWM_CALIB_" + str(jobData.jobID) + "_" + str(jobData.gageIDs[gageCheck])
+            testDF = jobs.query("JOB_NAME == '" + jobNameCheck + "'")
+            if len(testDF) != 0:
+                jobData.errMsg = "ERROR: Job ID: " + str(jobData.jobID) + \
+                                 " is already being ran under owner: " + \
+                                 str(jobData.owner) + ". User: " + \
+                                 str(userTmp) + " is attempting to initiate the workflow."
+                print "ERROR: You are attempting to initiate a job that is already being " + \
+                      "ran by user: " + str(jobData.owner)
+                raise Exception()
+            testDF = jobs.query("JOB_NAME == '" + jobNameCheck2 + "'")
+            if len(testDF) != 0:
+                jobData.errMsg = "ERROR: Job ID: " + str(jobData.jobID) + \
+                                 " is already being ran under owner: " + \
+                                 str(jobData.owner) + ". User: " + \
+                                 str(userTmp) + " is attempting to initiate the workflow."
+                print "ERROR: You are attempting to initiate a job that is already being " + \
+                      "ran by user: " + str(jobData.owner)
+                raise Exception()
                 
 def checkBasJob(jobData,gageNum):
     """
