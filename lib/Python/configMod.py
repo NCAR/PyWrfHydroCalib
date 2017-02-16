5cdfccf83024aa83fcdc5b534dfafbe88fa21ec8
Logan Karsten <karsten@vpn55.ucar.edu>
2017-02-15 21:21:30 -0700

    Update 2/15/17

--- lib/Python/configMod.py
+++ lib/Python/configMod.py
@@ -242,11 +242,12 @@ def createJob(argsUser):
         raise Exception()
         
     # Check entries into the config file to make sure they make sense.
-    try:
-        checkConfig(parser)
-    except:
-        print "ERROR: Improper Entries Into Config File."
-        raise
+    checkConfig(parser)
+    #try:
+    #    checkConfig(parser)
+    #except:
+    #    print "ERROR: Improper Entries Into Config File."
+    #    raise
         
     # Initialize job object
     jobObj = jobMeta()
