1485d378df79708cfce874f204075f3f6c41f1f2
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-03-16 15:03:35 -0600

    Update 3/16/17

--- validation.py
+++ validation.py
@@ -241,7 +241,6 @@ def main(argv):
             print jobData.gages[basin]
             print jobData.gageIDs[basin]
             validMod.runModelCtrl(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin,libPathTop)
-            sys.exit(1)
             #try:
             #    validMod.runModel(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin,run,libPathTop)
             #except:
