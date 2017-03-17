8714156563744cc4d6e41a20f22f05f04ac58724
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-03-17 17:36:38 -0600

    Update 3/17/17

--- validation.py
+++ validation.py
@@ -233,37 +233,36 @@ def main(argv):
         # If output is not complete, the model is still running, status stays at 0.5.
         # If job is not running, and output has been completed, status goes to 1.0.
         # This continues indefinitely until statuses for ALL basins go to 1.0.
-        #for basin in range(0,len(jobData.gages)):
-        for basin in range(0,1):
+        for basin in range(0,len(jobData.gages)):
             # First simulation will be the control simulation with default
             # parameters specified by the user at the beginning of the calibration
             # process.
             print jobData.gages[basin]
             print jobData.gageIDs[basin]
-            validMod.runModelCtrl(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin,libPathTop)
-            #try:
-            #    validMod.runModel(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin,run,libPathTop)
-            #except:
-            #    errMod.errOut(jobData)
-            time.sleep(30)
+            #validMod.runModelCtrl(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin,libPathTop)
+            try:
+                validMod.runModelCtrl(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin,libPathTop)
+            except:
+                errMod.errOut(jobData)
+            time.sleep(3)
             
-            validMod.runModelBest(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin)
-            #try:
-            #    validMod.runModelBest(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin)
-            #except:
-            #    errMod.errOut(jobData)
-            #time.sleep(30)
+            #validMod.runModelBest(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin)
+            try:
+                validMod.runModelBest(jobData,staticData,db,jobData.gageIDs[basin],jobData.gages[basin],keySlot,basin)
+            except:
+                errMod.errOut(jobData)
+            time.sleep(3)
                 
         # Check to see if program requirements have been met.
-        #if keySlot.sum() == entryValue:
-        #    jobData.spinComplete = 1
-        #    try:
-        #        db.updateSpinupStatus(jobData)
-        #    except:
-        #        errMod.errout(jobData)
-        #    jobData.genMsg = "VALIDATION FOR JOB ID: " + str(jobData.jobID) + " COMPLETE."
-        #    errMod.sendMsg(jobData)
-        #    completeStatus = True
+        if keySlot.sum() == entryValue:
+            jobData.spinComplete = 1
+            try:
+                db.updateSpinupStatus(jobData)
+            except:
+                errMod.errout(jobData)
+            jobData.genMsg = "VALIDATION FOR JOB ID: " + str(jobData.jobID) + " COMPLETE."
+            errMod.sendMsg(jobData)
+            completeStatus = True
     
 if __name__ == "__main__":
     main(sys.argv[1:])
