45974ab0183aa7010b469e741423002e17267ec9
Logan Karsten <karsten@pequin.rap.ucar.edu>
2017-07-18 17:41:37 -0600

    Update 7/18/17

--- lib/Python/calibMod.py
+++ lib/Python/calibMod.py
@@ -837,7 +837,7 @@ def runModel(statusData,staticData,db,gageID,gage,keySlot,basinNum,iteration):
         if statusData.jobRunType == 4:
             cmd = workDir + "/run_NWM_CALIB.sh"
             print cmd
-            p2 = subprocess.Popen([cmd],shell=True,stdout=subprocess.PIPE,stdout=subprocess.PIPE)
+            p2 = subprocess.Popen([cmd],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
             dump = p2.communicate()
             #p2 = subprocess.Popen([cmd],shell=True,close_fds=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
             #sys.exit(1)
