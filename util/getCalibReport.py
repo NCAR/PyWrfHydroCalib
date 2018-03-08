7d72857422ff7a4b62d74401ec76381e83090232
Logan Karsten <karsten@pequin.rap.ucar.edu>
2018-03-07 17:00:12 -0700

    Updating utility program getCalibReport.py

--- util/getCalibReport.py
+++ util/getCalibReport.py
@@ -13,6 +13,7 @@ import argparse
 import sys
 import os
 import getpass
+import numpy as np
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -58,14 +59,17 @@ def main(argv):
 
     # Lookup database username/login credentials based on username
     # running program.
-    try:
-        pwdTmp = getpass.getpass('Enter Database Password: ')
-        jobData.dbUName= 'WH_Calib_rw'
-        jobData.dbPwd = str(pwdTmp)
-    except:
-        print "ERROR: Unable to authenticate credentials for database."
-        sys.exit(1)
-        
+    #try:
+    #    pwdTmp = getpass.getpass('Enter Database Password: ')
+    #    jobData.dbUName= 'WH_Calib_rw'
+    #    jobData.dbPwd = str(pwdTmp)
+    #except:
+    #    print "ERROR: Unable to authenticate credentials for database."
+    #    sys.exit(1)
+    jobData.dbPwd = 'IJustWannaCalibrate'
+    jobData.dbUName= 'WH_Calib_rw'
+    jobData.port = 5432    
+    
     if not args.hostname:
         # We will assume localhost for Postgres DB
         hostTmp = 'localhost'
@@ -103,7 +107,12 @@ def main(argv):
     # Loop through each basin. Determine if which iteration we are on, then report the status
     # of the job for this basin.
     msgOut = ''
+    iterArray = np.empty([int(jobData.nIter)],np.int)
+    completeArray = np.empty([int(jobData.nIter)],np.float)
+
     for basin in range(0,len(jobData.gages)):
+	iterArray[:] = 0
+	completeArray[:] = 0.0
         keyStatus = 0.0
         keyStatusPrev = 0.0
         # First pull the unique ID for the basin. 
@@ -115,29 +124,31 @@ def main(argv):
         statusData = db.iterationStatus(jobData,domainID,str(jobData.gages[basin]))
         for iteration in range(0,int(jobData.nIter)):
             keyStatus = float(statusData[iteration][1])
-            if keyStatus == 1.0:
-                if iterComplete == int(jobData.nIter):
-                    msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
-                             ": CALIBRATION COMPLETE.\n"
-                else:
-                    iterComplete = iterComplete + 1
-            elif keyStatus == 0.0 and keyStatusPrev == 1.0:
+            iterationTmp = int(statusData[iteration][0])
+	    iterArray[iteration] = iterationTmp
+            completeArray[iteration] = keyStatus
+
+	indComplete = np.where(completeArray == 1)
+	indCheck1 = np.where(completeArray != 1.0)
+	indCheck2 = np.where(completeArray == 0.0)
+	if len(indComplete[0]) == int(jobData.nIter):
+	    msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
+                     ": CALIBRATION COMPLETE.\n"
+	else:
+            if len(indCheck2[0]) == int(jobData.nIter):
                 msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
-                         " - IS READY TO BEGIN ITERATION: " + str(iteration+1) + "\n " 
+                         " - HAS NOT BEGUN CALIBRATION.\n"
             else:
-                if keyStatusPrev == 0.0 and iteration == 0 and keyStatus == 0.0:
-                    msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
-                             " - HAS NOT BEGUN CALIBRATION.\n"
-                if keyStatusPrev == 1.0 and iteration != 0:
-                    msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
-                             ": " + str(msgDict[str(keyStatus)]) + \
-                             " - ITERATION: " + str(iteration+1) + "\n"
-                if keyStatusPrev == 0.0 and iteration == 0 and keyStatus != 0.0:
-                    msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
-                             ": " + str(msgDict[str(keyStatus)]) + \
-                             " - ITERATION: " + str(iteration+1) + "\n"
-            keyStatusPrev = keyStatus
-                         
+                iterLastComplete = len(indComplete[0])
+		iterCurrent = iterLastComplete + 1
+		indCurrent = np.where(iterArray == iterCurrent)
+		statusCurrent = completeArray[indCurrent[0][0]]
+                ind2 = np.where(completeArray != 0.0) and np.where(completeArray != 1.0)
+                iterTmp = iterArray[ind2[0][0]]
+		msgOut = msgOut + "BASIN: " + str(jobData.gages[basin]) + \
+		         ": " + str(msgDict[str(statusCurrent)]) + \
+		         " - ITERATION: " + str(iterCurrent) + "\n"
+                  
     jobData.genMsg = msgOut
     if int(args.contactFlag[0]) == 0:
         print jobData.genMsg
