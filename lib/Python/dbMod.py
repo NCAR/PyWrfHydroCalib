32d9ced7664194771fcbc4d3be4e5c302fcf22fc
Logan Karsten <karsten@pequin.local>
2018-08-22 17:25:35 -0600

    Bux fixes for dbMod

--- lib/Python/dbMod.py
+++ lib/Python/dbMod.py
@@ -212,7 +212,7 @@ class Database(object):
                  "valid_complete,acct_key,que_name,num_cores_model,num_nodes_model,\"num_cores_R\",\"num_nodes_R\"," + \
                  "job_run_type,exe,num_gages,owner,email," + \
                  "slack_channel,slack_token,slack_user,analysis_run_type,que_name_analysis) values " + \
-                 "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');" % (jobDir,jobData.bSpinDate.strftime('%Y-%m-%d'),\
+                 "('%s','%s','%s','%s',%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');" % (jobDir,jobData.bSpinDate.strftime('%Y-%m-%d'),\
                  jobData.eSpinDate.strftime('%Y-%m-%d'),0,jobData.sensFlag,jobData.sensTbl,jobData.nSensSample,\
                  jobData.nSensIter,jobData.nSensBatch,jobData.bSensDate.strftime('%Y-%m-%d'),\
                  jobData.eSensDate.strftime('%Y-%m-%d'),jobData.bSensEvalDate.strftime('%Y-%m-%d'),0,\
@@ -221,7 +221,7 @@ class Database(object):
                  jobData.nIter,0,jobData.bValidDate.strftime('%Y-%m-%d'),\
                  jobData.eValidDate.strftime('%Y-%m-%d'),jobData.bValidEvalDate.strftime('%Y-%m-%d'),\
                  0,jobData.acctKey,jobData.queName,jobData.nCoresMod,jobData.nNodesMod,jobData.nCoresR,jobData.nNodesR,\
-                 "-9999",jobData.jobRunType,jobData.exe,len(jobData.gages),\
+                 jobData.jobRunType,jobData.exe,len(jobData.gages),\
                  jobData.owner,emailStr,slStr1,slStr2,slStr3,jobData.analysisRunType,jobData.queNameAnalysis)
         try:
             self.dbCursor.execute(sqlCmd)
