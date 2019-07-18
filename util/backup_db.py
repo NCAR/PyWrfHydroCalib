d69baee00dbf73b7e3ece3e3ff2232c38cfd103a
logankarsten <karsten@ucar.edu>
2019-07-18 11:17:58 -0600

    Expanding DB backup.

--- util/backup_db.py
+++ util/backup_db.py
@@ -133,7 +133,7 @@ def main(argv):
                 sys.exit(1)
 
             # Insert this information into the external database.
-            cmd = "INSERT INTO \"Domain_Meta\" (domainID,gage_id,link_id,domain_path,gage_agency,geo_e," + \
+            cmd = "INSERT INTO \"Domain_Meta\" (\"domainID\",gage_id,link_id,domain_path,gage_agency,geo_e," + \
                   "geo_w,geo_s,geo_n,hyd_e,hyd_w,hyd_s,hyd_n,geo_file,land_spatial_meta_file,wrfinput_file," + \
                   "soil_file,fulldom_file,rtlink_file,spweight_file," + \
                   "gw_file,gw_mask,lake_file,forcing_dir,obs_file,site_name,lat,lon," + \
@@ -169,23 +169,193 @@ def main(argv):
     # 3.) Loop through the calibration tables and either enter information, or update
     #     fields that need to be updated.
     for expTmp in resultsExp:
-        cmd = "select \"jobID\" from \"Job_Meta\" where \"jobID\"='" + str(expTmp) + "';"
+        cmd = "select \"jobID\" from \"Job_Meta\" where \"jobID\"='" + str(expTmp[0]) + "';"
         try:
-            dbCursorIn.execute(cmd)
-            results = dbCursorIn.fetchone()
+            dbCursorExt.execute(cmd)
+            resultsTmp = dbCursorExt.fetchone()
         except:
             print("Unable to query job ID: " + expTmp + " from external postgres database.")
             sys.exit(1)
 
-        if len(results) == 0:
+        if resultsTmp == None:
             # We haven't entered in job meta information.
             cmd = "select * from \"Job_Meta\" where \"jobID\"='" + str(expTmp) + "';"
             try:
                 dbCursorIn.execute(cmd)
-                results = dbCursorIn.fetchone()
+                resultsTmp = dbCursorIn.fetchone()
             except:
-                print("Unable to query job ID: " + str(expTmp) + " from: " + dbInPath)
+                print("Unable to query job ID: " + str(expTmp[0]) + " from: " + dbInPath)
                 sys.exit(1)
+
+            # Sanity checking here.
+            if len(resultsTmp) != 1:
+                print("Unexpected multiple job entries for ID: " + str(expTmp[0]) +
+                      " in external database file")
+                sys.exit(1)
+            if len(resultsTmp[0]) != 31:
+                print("Unexpected job information for ID: " + str(expTmp[0]) +
+                      " in external database file")
+                sys.exit(1)
+
+            # Insert this information into the external Job_Meta table.
+            cmd = "insert into \"Job_Meta\" (\"jobID\",\"Job_Directory\",date_su_start,date_su_end," + \
+                  "su_complete,date_calib_start,date_calib_end,date_calib_start_eval,num_iter," + \
+                  "iter_complete,calib_complete,valid_start_date,valid_end_date,valid_start_date_eval," + \
+                  "valid_complete,acct_key,que_name,num_cores_model,num_nodes_model,\"num_cores_R\",\"num_nodes_R\"," + \
+                  "sql_host,job_run_type,exe,num_gages,owner,email," + \
+                  "slack_channel,slack_token,slack_user,analysis_run_type,que_name_analysis) values " + \
+                  "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'," \
+                  "'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'," \
+                  "'%s');" % (expTmp[0],resultsTmp[0][1],resultsTmp[0][2],resultsTmp[0][3],resultsTmp[0][4],
+                              resultsTmp[0][5],resultsTmp[0][6],resultsTmp[0][7],resultsTmp[0][8],
+                              resultsTmp[0][9],resultsTmp[0][10],resultsTmp[0][11],resultsTmp[0][12],
+                              resultsTmp[0][13],resultsTmp[0][14],resultsTmp[0][15],resultsTmp[0][16],
+                              resultsTmp[0][17],resultsTmp[0][18],resultsTmp[0][19],resultsTmp[0][20],resultsTmp[0][21],
+                              resultsTmp[0][22],resultsTmp[0][23],resultsTmp[0][24],resultsTmp[0][25],resultsTmp[0][26],
+                              resultsTmp[0][27],resultsTmp[0][28],resultsTmp[0][29],resultsTmp[0][30])
+
+            try:
+                dbCursorExt.execute(cmd)
+                connExt.commit()
+            except:
+                print("Unable to enter in job meta information for basin ID: " + str(expTmp[0]) +
+                      " from external database.")
+                sys.exit(1)
+
+        # Now, either enter information for calibration/sensitivity/validation into the various tables holding
+        # the data. If the entry has already been created, update it. Otherwise, create a new entry.
+        cmd = "select * from \"Sens_Params\" where \"jobID\"='" + str(expTmp[0]) + "';"
+        try:
+            dbCursorIn.execute(cmd)
+            resultsTmp = dbCursorIn.fetchall()
+        except:
+            print("Unable to query Sens_Params for job ID: " + expTmp + " from sqlite file.")
+            sys.exit(1)
+        if len(resultsTmp) != 0:
+            # We have data to either enter into the postgres DB, or need to update.
+            for entryTmp in resultsTmp:
+                bsnIdUnique = int(str(entryTmp[0][0]) + str(entryTmp[0][1]))
+                # First check to see if we need to run an INSERT or an UPDATE.
+                cmd = "select * from \"Sens_Params\" where \"jobID\"=" + entryTmp[0][0] + " and \"domainID\"=" + \
+                      str(bsnIdUnique) + " and iteratin=" + entryTmp[0][2] + " and paramName='" + entryTmp[0][3] + \
+                      "' and paramValue=" + entryTmp[0][4]
+                try:
+                    dbCursorExt.execute(cmd)
+                    updatesTmp = dbCursorExt.fetchone()
+                except:
+                    print("Unable to run query on Sens_Params external.....")
+                    sys.exit(1)
+                if updatesTmp == None:
+                    # This is a new entry, we need run an INSERT.
+                    cmd = "insert into \"Sens_Params\" (\"jobID\",\"domainID\",iteration,\"paramName\",\"paramValue\") " \
+                          "values ('%s','%s','%s','%s','%s'); " % (updatesTmp[0][0],str(bsnIdUnique),updatesTmp[0][2],
+                                                                   updatesTmp[0][3],updatesTmp[0][4])
+                    try:
+                        dbCursorExt.execute(cmd)
+                        connExt.commit()
+                    except:
+                        print("Unable to insert data into external Sens_Params table.")
+                        sys.exit(1)
+                else:
+                    # We are running an UPDATE on an existing entry.
+                    cmd = "update \"Sens_Params\" set \"jobID\"='" + updatesTmp[0][0] + "', \"domainID\"='" + \
+                          str(bsnIdUnique) + "', iteration='" + updatesTmp[0][2] + "', \"paramName\"='" + updatesTmp[0][3] + \
+                          "', \"paramValue\"='" + updatesTmp[0][4] + "' where \"jobID\"='" + updatesTmp[0][0] + \
+                          "', and \"domainID\"='" + str(bsnIdUnique) + "', and iteration='" + updatesTmp[0][2] + \
+                          "' and \"paramName\"='" + updatesTmp[0][3] + "' and \"paramValue\"='" + updatesTmp[0][4] + "';"
+                    try:
+                        dbCursorExt.execute(cmd)
+                        connExt.commit()
+                    except:
+                        print("Unable to update entry in external Sens_Params table.")
+                        sys.exit(1)
+
+
+
+        cmd = "select * from \"Sens_Stats\" where \"jobID\"='" + str(expTmp[0]) + "';"
+        try:
+            dbCursorIn.execute(cmd)
+            resultsTmp = dbCursorIn.fetchall()
+        except:
+            print("Unable to query Sens_Stats for job ID: " + expTmp + " from sqlite file.")
+            sys.exit(1)
+
+
+
+
+
+        cmd = "select * from \"Calib_Params\" where \"jobID\"='" + str(expTmp[0]) + "';"
+        try:
+            dbCursorIn.execute(cmd)
+            resultsTmp = dbCursorIn.fetchall()
+        except:
+            print("Unable to query Calib_Params for job ID: " + expTmp + " from sqlite file.")
+            sys.exit(1)
+        if len(resultsTmp) != 0:
+            # We have data to either enter into the postgres DB, or need to update.
+            for entryTmp in resultsTmp:
+                bsnIdUnique = int(str(entryTmp[0][0]) + str(entryTmp[0][1]))
+                # First check to see if we need to run an INSERT or an UPDATE.
+                cmd = "select * from \"Calib_Params\" where \"jobID\"=" + entryTmp[0][0] + " and \"domainID\"=" + \
+                      str(bsnIdUnique) + " and iteratin=" + entryTmp[0][2] + " and paramName='" + entryTmp[0][3] + \
+                      "' and paramValue=" + entryTmp[0][4]
+                try:
+                    dbCursorExt.execute(cmd)
+                    updatesTmp = dbCursorExt.fetchone()
+                except:
+                    print("Unable to run query on Calib_Params external.....")
+                    sys.exit(1)
+                if updatesTmp == None:
+                    # This is a new entry, we need run an INSERT.
+                    cmd = "insert into \"Calib_Params\" (\"jobID\",\"domainID\",iteration,\"paramName\",\"paramValue\") " \
+                          "values ('%s','%s','%s','%s','%s'); " % (updatesTmp[0][0],str(bsnIdUnique),updatesTmp[0][2],
+                                                                   updatesTmp[0][3],updatesTmp[0][4])
+                    try:
+                        dbCursorExt.execute(cmd)
+                        connExt.commit()
+                    except:
+                        print("Unable to insert data into external Calib_Params table.")
+                        sys.exit(1)
+                else:
+                    # We are running an UPDATE on an existing entry.
+                    cmd = "update \"Calib_Params\" set \"jobID\"='" + updatesTmp[0][0] + "', \"domainID\"='" + \
+                          str(bsnIdUnique) + "', iteration='" + updatesTmp[0][2] + "', \"paramName\"='" + updatesTmp[0][3] + \
+                          "', \"paramValue\"='" + updatesTmp[0][4] + "' where \"jobID\"='" + updatesTmp[0][0] + \
+                          "', and \"domainID\"='" + str(bsnIdUnique) + "', and iteration='" + updatesTmp[0][2] + \
+                          "' and \"paramName\"='" + updatesTmp[0][3] + "' and \"paramValue\"='" + updatesTmp[0][4] + "';"
+                    try:
+                        dbCursorExt.execute(cmd)
+                        connExt.commit()
+                    except:
+                        print("Unable to update entry in external Calib_Params table.")
+                        sys.exit(1)
+
+
+
+
+        cmd = "select * from \"Calib_Stats\" where \"jobID\"='" + str(expTmp[0]) + "';"
+        try:
+            dbCursorIn.execute(cmd)
+            resultsTmp = dbCursorIn.fetchall()
+        except:
+            print("Unable to query Calib_Stats for job ID: " + expTmp + " from sqlite file.")
+            sys.exit(1)
+
+
+
+
+
+
+        cmd = "select * from \"Valid_Stats\" where \"jobID\"='" + str(expTmp[0]) + "';"
+        try:
+            dbCursorIn.execute(cmd)
+            resultsTmp = dbCursorIn.fetchall()
+        except:
+            print("Unable to query Valid_Stats for job ID: " + expTmp + " from sqlite file.")
+            sys.exit(1)
+
+
+
 
     # Close the sqlite connection.
     try:
