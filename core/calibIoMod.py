e930e5ed55e7185e79b80eb4b251f8c53c921f4c
arezoorn <arezoo.rafieeinasab@gmail.com>
2020-04-27 14:49:50 -0600

    adding the multiSite option

--- core/calibIoMod.py
+++ core/calibIoMod.py
@@ -755,6 +755,22 @@ def setupModels(jobData,db,args,libPathTop):
                jobData.errMsg = "ERROR: Failure creating the symlink to the mask"
                raise
 
+        # Create symlink for the calib_sites.csv file if runnig calibration and enableMultiSites = 1
+
+        if jobData.calibFlag == 1 and jobData.enableMultiSites == 1 :
+           try:
+               maskFile = gageData.forceDir[0:-7] + "calib_sites.csv"
+               link = gageDir + "/RUN.CALIB/calib_sites.csv"
+               os.symlink(maskFile,link)
+               link = gageDir + "/RUN.VALID/calib_sites.csv"
+               os.symlink(maskFile,link)
+
+           except:
+               jobData.errMsg = "ERROR: Failure creating the symlink to the calib_sites.csv which is required in the case of enableMultiSites = 1"
+               raise
+
+
+
 def generateCalibGroupScript(jobData,groupNum,scriptPath,topDir):
     """
     Function to generate the run script for a particular group of basins.
