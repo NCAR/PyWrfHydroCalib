dd2b8f9807fe70e2945b7d635bb590ebbb2599a3
arezoorn <arezoo.rafieeinasab@gmail.com>
2020-12-07 12:25:42 -0700

    fixing the nnse calculation

--- core/calib_workflow.R
+++ core/calib_workflow.R
@@ -276,7 +276,7 @@ if (cyclecount > 0) {
       nse = hydroGOF::NSE(q_cms, obs, na.rm=TRUE, FUN=NULL, epsilon="Pushpalatha2012"),
       nselog = NseLogM(q_cms, obs), #consider adding constant value to station with the occurrence of zero flows 
       nsewt = NseWtM(q_cms, obs), #consider adding constant value to station with the occurrence of zero flows
-      nnse = 1 / (2 - nse),
+      nnse = NNse(q_cms, obs),
       nnsesq = NNseSq(q_cms, obs), 
       kge = hydroGOF::KGE(q_cms, obs, na.rm=TRUE, method="2009", out.type="single"), # Gupta et al (2009) is the basis of lbeprime 
       hyperResMultiObj = hyperResMultiObj(q_cms, obs, na.rm=TRUE),
