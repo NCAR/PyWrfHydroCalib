b6c7c6639a671bbb52c2a498674bb09a8efdb6b1
arezoorn <arezoo.rafieeinasab@gmail.com>
2021-04-26 15:17:17 -0600

    fixing the number of parameters in the parameter table file

--- core/dbMod.py
+++ core/dbMod.py
@@ -793,7 +793,7 @@ class Database(object):
         if jobData.calibFlag == 1:
             # Open parameter table and read values in.
             tblData = pd.read_csv(jobData.calibTbl)
-            if len(tblData) != 45:
+            if len(tblData) != 44:
                 jobData.errMsg = "ERROR: Unexpected calibration parameter table format."
                 raise Exception()
             
